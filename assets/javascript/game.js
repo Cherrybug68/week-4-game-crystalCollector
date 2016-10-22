7d5d7ad226053daa31ce34a9e9ccf0254a624d7b
Shari Smith <Cherrybug68@gmail.com>
2016-10-22 16:26:52 -0400

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,13 +7,12 @@ var numberToMatch = [];
 
 var counter = 0;
 
-var crystalNumbers = [];
-
 var crystals = ["assets/images/BlueCrystal.jpg",
 				"assets/images/GreenCrystal.jpg",
 				"assets/images/PurpleCrystal.jpg",
 				"assets/images/RedCrystal.jpg"];
 
+var start = false;
 
 
 // create random number to guess
@@ -26,8 +25,11 @@ function createNumberToMatch () {
 // create crystal random numbers
 function createCrystalNumbers () {
 
+		crystalNumbers =[];
+
 		for (var i = 0; i < crystals.length; i++) {
-			// crystalNumbers.push(Math.floor(Math.random() * (12 - 1) + 1));
+
+			crystalNumbers.push(Math.floor(Math.random() * (12 - 1) + 1));
 			
 			var imageCrystal = $('<img>');
 
@@ -39,29 +41,51 @@ function createCrystalNumbers () {
 
 			imageCrystal.addClass('crystalImage');
 
-			$('#crystals').append(imageCrystal);
+			$('.crystalImage').append(imageCrystal);
 			
 		}
-		// console.log(crystalNumbers);
+		console.log(crystalNumbers);
 }
 
 function startGame () {
-	counter = 0;
-	$('#yourNumber').text(counter);
 
-	function randomIntFromInterval(min,max){
-		return Math.floor(Math.random()*(max-min+1)+min);
+	$("img").on('click', function(){
+
+	if (start == true)
+	{
+	counter += parseInt($(this).data('num'));
+	console.log(numberToMatch);
+
+		$("#yourNumber").text(counter);
+
+		// checking to see if you won/loss
+		if (counter == numberToMatch) {
+			$("#wonLoss").html("You Won!");
+			start = false;
+		}
+
+		else if (counter > numberToMatch){
+			$("#wonLoss").html("You Lost!");
+			start = false;
+		}
 	}
+	});
 }
 
-createNumberToMatch();
-createCrystalNumbers();
+$("#startButton").on('click', function(){
+	start = true;
+	reset();
+});
+
+function reset(){
+	counter = 0;
+	numberToMatch = 0;
+	$("#wonLoss").html("");
+	$("#numberToMatch").html("");
+	$(".crystalImage").empty();
+	createNumberToMatch();
+	createCrystalNumbers();
+	startGame();
+}
 
 
-// // checking to see if you won/loss
-// if (counter == numberToMatch) {
-// 	$("#wonLoss").html = "You Won!"
-// }
-// else if (counter > numberToMatch){
-// 	$("#wonLoss").html = "You Lost!";
-// }
